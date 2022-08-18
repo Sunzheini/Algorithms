2a42cdb1f87cb571e4cc2ef277d5ac205b68c682
Daniel Zorov <daniel_zorov@abv.bg>
2022-08-18 17:29:51 +0300

    Update180822

---
+++ lab/l040722.py
@@ -0,0 +1,28 @@
+# Recursion
+
+# T(n) = 3(n**2) + 3n + 3
+# higher terms dominate lower terms: n > 2, n**2 > n, n**3 > n**2
+
+# worst case / best case / average case
+
+# 3 asymptotic notations: Big O, Big Omega, Big Theta
+
+# ------------------------------------------------------------------
+ # Recursion # -2.38h
+
+
+
+
+# ------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
